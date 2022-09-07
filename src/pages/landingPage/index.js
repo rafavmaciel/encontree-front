d2103e48c42b2af9d65748cf6f72761fe765da22
Michel Oliveira <micheltdoliveira@gmail.com>
2022-09-07 02:07:04 -0300

    add header e landingPage base

---
+++ src/pages/landingPage/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from '../../components/header/index';
+
+
+function LandingPage() {
+    return (
+        <div className="App">
+            <Header/>
+
+        </div>
+    );
+  }
+  
+  export default LandingPage;
