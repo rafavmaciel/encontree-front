fb79e9f94da679539d1bc6f61d6d711a1e2e90d0
Itamar Bernardo <itamarbernardo2013@gmail.com>
2022-09-15 16:24:38 -0300

    Adicionado pagina de menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from './pages/landingPage/index';
 import Login from './pages/login/index';
+import Menu from './pages/menu/index';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/menu" element={<Menu/>}/>
 
       </Routes>
     </BrowserRouter>
---
+++ src/pages/menu/index.js
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../../components/header/index';
+import './styles.scss'
+
+function Menu() {
+    return (
+    <>
+        <Header/>
+
+        <div className="container mt-5" >
+            <button type="submit" className="botao light_blue botao_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+                Procurar Imóvel
+            </button>
+            <button type="submit" className="botao light_blue mt-5 ">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                    <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                </svg>
+                Anunciar
+            </button>
+            <button type="submit" className="botao light_blue mt-5 botao_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-fill" viewBox="0 0 16 16">
+                    <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                </svg>
+                Gerenciar Anúncios
+            </button>
+
+        </div>
+             
+
+    </>
+    );
+  }
+  
+  export default Menu;
