2597924f43de1a8114350c6c68727342a7c678b5
Itamar Bernardo <itamarbernardo2013@gmail.com>
2022-09-15 15:28:54 -0300

    finalizado pagina de landing-page

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -8,15 +8,17 @@ function LandingPage() {
     <>
         <Header/>
 
-        <div className="card mb-3 cotainer col-lg-8 offset-lg-2" >
+        <div className="card mb-3 container" >
             <div className="row no-gutters">
-                <div className="col-md-8">
+                <div className="col-md-7">
                     <div className="card-body">
                         <h5 className="card-title">Bem-vindo ao Encontree</h5>
-                        <p className="card-text">Agora você vai poder encontrar, de forma fácil e rápida, imóveis próximos da sua faculdade. Filtre por diversas categorias e encontre a moradia ideal para você ou se já possui um local, anuncie para que outras pessoas possam morar com você e dividir os custos.</p>
+                        <p className="card-text">Agora você vai poder encontrar, de forma fácil e rápida, imóveis próximos da sua faculdade. Filtre por diversas categorias e encontre a moradia ideal para você ou se já possui um local, anuncie para que outras pessoas possam morar com você e dividir os custos. </p>
+                        <button type="submit" className="botao white mt-5">Cadastre-se Gratuitamente</button>
+
                     </div>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-5">
                     <img src={img_lp} className="card-img"  />
                 </div>
                 
