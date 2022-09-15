580a70aa4b76284228b7790981bc7d9b7bda109d
Michel Oliveira <micheltdoliveira@gmail.com>
2022-09-14 21:24:59 -0300

    centralizando card landing-page

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -8,19 +8,19 @@ function LandingPage() {
     <>
         <Header/>
 
-        <div className="card mb-3 cotainer" >
-        <div className="row no-gutters">
-            <div className="col-md-8">
-            <div className="card-body">
-                <h5 className="card-title">Bem-vindo ao Encontree</h5>
-                <p className="card-text">Agora você vai poder encontrar, de forma fácil e rápida, imóveis próximos da sua faculdade. Filtre por diversas categorias e encontre a moradia ideal para você ou se já possui um local, anuncie para que outras pessoas possam morar com você e dividir os custos.</p>
+        <div className="card mb-3 cotainer col-lg-8 offset-lg-2" >
+            <div className="row no-gutters">
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Bem-vindo ao Encontree</h5>
+                        <p className="card-text">Agora você vai poder encontrar, de forma fácil e rápida, imóveis próximos da sua faculdade. Filtre por diversas categorias e encontre a moradia ideal para você ou se já possui um local, anuncie para que outras pessoas possam morar com você e dividir os custos.</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <img src={img_lp} className="card-img"  />
+                </div>
+                
             </div>
-            </div>
-            <div className="col-md-4">
-            <img src={img_lp} className="card-img"  />
-            </div>
-            
-        </div>
         </div>
 
     </>
