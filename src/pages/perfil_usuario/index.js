20e4dc0f39d0aa174f56fbca5949b762b89c2601
Itamar Bernardo <itamarbernardo2013@gmail.com>
2022-09-17 18:39:52 -0300

    Adicionado imagem para perfil de usuario

--- src/pages/perfil_usuario/index.js
+++ src/pages/perfil_usuario/index.js
@@ -9,6 +9,7 @@ function PerfilUsuario() {
     const sobre = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id nulla finibus, bibendum ex cursus, dictum arcu. Sed sollicitudin, dui et fermentum sagittis.'
     const tags = 'gosta de gatos, não aceita fumantes, tranquila'
     const preferencias = 'Manter a moradia limpa é essencial, não gosto de bagunça'
+    const img_url = 'https://anagiovanna.com.br/blog/wp-content/uploads/2022/01/foto-para-perfil-do-whatsapp-21.jpg'
 
     return (
     <>
@@ -20,11 +21,9 @@ function PerfilUsuario() {
             </div>
 
             <div className='icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white" className="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                </svg>
-
+            <span className = "img_circle">
+                <img src={img_url} width='100' height='100' className="bi bi-person-circle" alt='Imagem perfil'/>
+                </span>
             </div>
             <div className='perfil'>
                 <h3>
