64f77b2a6ba198207c9da4f82a1f60151f350f34
unknown <antoniodurvalc@gmail.com>
2022-09-20 09:58:04 -0300

    removendo espaçamento no top

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -22,7 +22,7 @@ function Login() {
 
     return (
         <>
-            <div className="flex flex flex-col items-center justify-center min-h-screen py-2 ">
+            <div className="flex flex flex-col items-center justify-center mt-4">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
                     <div className="bg-white rounded-2xl shadow-2xl flex w-full max-w-4xl ">
                         <div className="w-3/5 p-5">
