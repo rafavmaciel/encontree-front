dd3f58c726341740db4133e58843ab8fb9a5b50d
Rafael <rafael.maciel@showtecnologia.com>
2022-12-16 09:27:10 -0300

    alterando a margem

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -34,7 +34,7 @@ function Header() {
         <nav className="bg-white px-2 sm:px-4 py-1 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 hover:border-b-4 hover:border-blue-700 transition duration-500 dark:border-gray-600">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Link to="/home" className="link-nostyle flex items-center">
-                    <i className="fas fa-home text-2xl text-blue-700 dark:text-blue-400"></i>
+                    <i className="fas fa-home text-2xl text-blue-700 dark:text-blue-400 ml-3"></i>
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                         Encontree
                     </span>
